bd6511f650c6e699911485dfa8950ef2fb243bff
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-10-21 10:50:44 +0300

    add routing and html pages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
+import { readFile } from "fs/promises";
 import http from "http";
 import { config } from "dotenv";
 
-const server = http.createServer((req, res) => {
+let html;
+
+const server = http.createServer(async (req, res) => {
+    if (req.url === "/" || req.url === "/about" || req.url === "/contact") {
+        if (req.url === "/") {
+            try {
+                html = await readFile("./src/pages/home.html");
+            } catch (err) {
+                console.log(err);
+            }
+        } else if (req.url === "/about") {
+            try {
+                html = await readFile("./src/pages/about.html");
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            try {
+                html = await readFile("./src/pages/contact.html");
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    } else {
+        try {
+            html = await readFile("./src/pages/404.html");
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(200);
+    res.write(html);
     res.end();
 });
 
